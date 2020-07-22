413680cf84b726b4f5d3fd0eb2a501a11d977205
nkgw <s1832099lh@s.chibakoudai.jp>
2020-07-22 17:11:34 +0900

    Update final.js

--- webgl/final.js
+++ webgl/final.js
@@ -88,17 +88,19 @@ var init = function() {
   scene.add(earth);
   earth.scale.set(2.5,2.5,2.5);
 
-for(let i=0; i<10; i++){
   //立方体
-  var cubegeometry = new THREE.BoxGeometry( 1, 1, 1, 1, 1, 1);
+  
+  for(let i=0; i<10; i++){
+    var cubegeometry = new THREE.BoxGeometry( 1, 1, 1, 1, 1, 1);
+    
+    var radian = i/8*(Math.PI)*2;
+    cube.position.set(-3.5* Math.cos(radian),-0.3,-3.5*Math.sin(radian));
+    cube.receiveShadow = true;
+    cube.castShadow = true;
+    round3_group.add( cube );
+    cube.scale.set(0.5,0.5,0.5);
+  }
   var cube = new THREE.Mesh( cubegeometry, mat );
-  var radian = i/8*(Math.PI)*2;
-  cube.position.set(-3.5* Math.cos(radian),-0.3,-3.5*Math.sin(radian));
-  cube.receiveShadow = true;
-  cube.castShadow = true;
-  round3_group.add( cube );
-  cube.scale.set(0.5,0.5,0.5);
-}
 
 for(let i=0; i<10; i++){
   //立方体

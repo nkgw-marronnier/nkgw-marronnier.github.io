db76b587aba778ada1321fd57915eea0deb3e247
nkgw <s1832099lh@s.chibakoudai.jp>
2020-07-24 14:22:15 +0900

    Update final.js

--- webgl/js/final.js
+++ webgl/js/final.js
@@ -15,7 +15,7 @@ var init = function () {
 
   // カメラを作成
   var camera = new THREE.PerspectiveCamera(45, width / height, 1, 200);
-  camera.position.set(0, 0, 50);
+  camera.position.set(0, 0, 25);
 
   // カメラコントローラーを作成
   const controls = new THREE.OrbitControls(camera, canvas);

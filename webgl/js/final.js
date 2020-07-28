24f921d1f720ed76530ae521ca4e92bd23770392
nkgw <s1832099lh@s.chibakoudai.jp>
2020-07-28 17:09:38 +0900

    Update final.js

--- webgl/js/final.js
+++ webgl/js/final.js
@@ -700,9 +700,9 @@ var init = function () {
   // set path to texture images
   // either relative or absolute path
   VolumetricFire.texturePath = '/textures/';
-  var fireWidth = 100;
-  var fireHeight = 400;
-  var fireDepth = 100;
+  var fireWidth = 5;
+  var fireHeight = 10;
+  var fireDepth = 10;
   var sliceSpacing = 10;
 
   var fire = new VolumetricFire(
